0776b6ce72514ba1a161a7b33be3517049afb190
lawand <lawand87@gmail.com>
2011-03-03 21:15:09 +0200

    made the version number 0.2.1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -599,7 +599,7 @@ void MainWindow::about()
     messageBox.setWindowTitle("Droper");
 
     messageBox.setText(
-            "<b>Droper v0.2</b> - "
+            "<b>Droper v0.2.1</b> - "
             "<a href=\"http://www.dropbox.com/\">Dropbox</a> Client."
             );
 
