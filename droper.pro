8447cc41a2e1e803db8e1832af078570d24bf54a
lawand <lawand87@gmail.com>
2011-04-14 18:13:12 +0300

    made the version number 0.3.1

--- droper.pro
+++ droper.pro
@@ -1,6 +1,6 @@
 QT       += core gui network svg
 
-VERSION = 0.3.0
+VERSION = 0.3.1
 
 TARGET = Droper
 TEMPLATE = app
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -806,7 +806,7 @@ void MainWindow::about()
     messageBox.setWindowTitle("Droper");
 
     messageBox.setText(
-            "<b>Droper v0.3.0</b> - "
+            "<b>Droper v0.3.1</b> - "
             "<a href=\"http://www.dropbox.com/\">Dropbox</a> Client."
             );
 
