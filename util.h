2e42b4b219380524078a733a6bb2a06bfa1ddea2
lawand <lawand87@gmail.com>
2012-05-12 02:00:44 +0200

    added license

--- util.h
+++ util.h
@@ -1,3 +1,27 @@
+/****************************************************************************
+**
+** Copyright 2011, 2012 Omar Lawand Dalatieh.
+**
+** This file is part of Droper.
+**
+** Droper is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** Droper is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Droper.  If not, see <http://www.gnu.org/licenses/>.
+**
+** For more information, check out the GNU General Public license found
+** in the COPYING file and the README file.
+**
+****************************************************************************/
+
 #ifndef UTIL_H
 #define UTIL_H
 
